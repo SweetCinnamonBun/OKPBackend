5689952910b13241c14df249206d8ff595687d98
Roman <91057166+SweetCinnamonBun@users.noreply.github.com>
2024-03-12 20:19:20 +0200

    new changesss

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using System.Text;
 using System.Text.Json.Serialization;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using OKPBackend.Data;
@@ -55,11 +58,30 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-DotNetEnv.Env.Load();
-string? db_password = Environment.GetEnvironmentVariable("db_password");
+// if (builder.Environment.IsProduction())
+// {
+//     var keyVaultURL = builder.Configuration.GetSection("KeyVault:KeyVaultURL");
+//     var keyVaultClientId = builder.Configuration.GetSection("KeyVault:ClientId");
+//     var keyVaultClientSecret = builder.Configuration.GetSection("KeyVault:ClientSecret");
+//     var keyVaultDirectoryID = builder.Configuration.GetSection("KeyVault:DirectoryID");
 
-string? connectionString = builder.Configuration.GetConnectionString("OKPConnectionString");
-connectionString = connectionString.Replace("{DatabasePassword}", db_password ?? "");
+//     var credential = new ClientSecretCredential(keyVaultDirectoryID.Value!.ToString(), keyVaultClientId.Value!.ToString(), keyVaultClientSecret.Value!.ToString());
+//     builder.Configuration.AddAzureKeyVault(keyVaultURL.Value!.ToString(), keyVaultClientId.Value!.ToString(), keyVaultClientSecret.Value!.ToString(), new DefaultKeyVaultSecretManager());
+
+//     var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), credential);
+
+//     builder.Services.AddDbContext<OKPDbContext>(options =>
+//     {
+//         options.UseSqlServer(client.GetSecret("ProdConnection").Value.Value.ToString());
+//     });
+
+// };
+
+// DotNetEnv.Env.Load();
+// string? db_password = Environment.GetEnvironmentVariable("db_password");
+
+// string? connectionString = builder.Configuration.GetConnectionString("OKPConnectionString");
+// connectionString = connectionString.Replace("{DatabasePassword}", db_password ?? "");
 
 
 builder.Services.AddCors(x => x.AddPolicy("corspolicy", build =>
@@ -67,7 +89,7 @@ builder.Services.AddCors(x => x.AddPolicy("corspolicy", build =>
     build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
 
-builder.Services.AddDbContext<OKPDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<OKPDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("OKPConnectionString")));
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
@@ -127,6 +149,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseCors("corspolicy");
 
