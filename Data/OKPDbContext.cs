f58166e3ec5a364131f3a113236417a27732bf07
Roman <91057166+SweetCinnamonBun@users.noreply.github.com>
2024-03-12 22:40:23 +0200

    fixing

--- Data/OKPDbContext.cs
+++ Data/OKPDbContext.cs
@@ -20,6 +20,10 @@ namespace OKPBackend.Data
 
         public DbSet<Favorite> Favorites { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:okpbackend-server.database.windows.net,1433;Initial Catalog=rk-db;Persist Security Info=False;User ID=okpbackend;Password=rakennustenkaupunki123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
         // protected override void OnModelCreating(ModelBuilder builder)
         // {
         //     base.OnModelCreating(builder);
